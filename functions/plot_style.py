ebf823b0259892f0596bd1b9441199b3aa1e81a6
Joshua <joshua.damm@kit.edu>
2025-08-29 16:27:50 +0200

    better plotting for thesis or paper

--- functions/plot_style.py
+++ functions/plot_style.py
@@ -1,37 +1,34 @@
 import matplotlib as mpl
 
 def set_plot_style(use_latex: bool = False):
-    """
-    Global plot parameters
-    :param use_latex: True → Use LaTeX, False → Use Mathtext/Computer Modern
-    """
     if use_latex:
         mpl.rcParams.update({
             "text.usetex": True,
             "font.family": "serif",
             "font.serif": ["Computer Modern"],
-            "axes.labelsize": 13,
-            "axes.titlesize": 14,
-            "xtick.labelsize": 12,
-            "ytick.labelsize": 12,
-            "legend.fontsize": 12,
+            "axes.labelsize": 14,   # was 13
+            "axes.titlesize": 16,   # was 14
+            "xtick.labelsize": 13,  # was 12
+            "ytick.labelsize": 13,
+            "legend.fontsize": 13,
             "lines.linewidth": 1.5,
-            "figure.dpi": 800,
+            "figure.dpi": 600,  # 600 is sufficient for print
         })
     else:
         mpl.rcParams.update({
             "text.usetex": False,
             "mathtext.fontset": "cm",
             "font.family": "serif",
-            "font.serif": ["Times New Roman"],
-            "axes.labelsize": 13,
-            "axes.titlesize": 14,
-            "xtick.labelsize": 12,
-            "ytick.labelsize": 12,
-            "legend.fontsize": 12,
+            "font.serif": ["Times New Roman"],  # matches many thesis guidelines
+            "axes.labelsize": 14,
+            "axes.titlesize": 16,
+            "xtick.labelsize": 13,
+            "ytick.labelsize": 13,
+            "legend.fontsize": 13,
             "lines.linewidth": 1.5,
-            "figure.dpi": 800,
+            "figure.dpi": 600,
         })
+
 
 def set_plot_style_UVVis(use_latex: bool = False):
     """
